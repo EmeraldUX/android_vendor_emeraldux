e82e6d9ae2d3b9e137c1a787e8c9a84b4bf5a71e
EmeraldUX <EmeraldUX@protonmail.com>
2020-05-05 15:11:48 +0200

    EmeraldUX Build Success

---
+++ tools/emeraldux.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Root method chooser for the Resurrection Remix ROM
+# Specify colors utilized in the terminal
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    ylw=$(tput setaf 3)             #  yellow
+    blu=$(tput setaf 4)             #  blue
+    txtrst=$(tput sgr0)             #  Reset
+    normal='tput sgr0'
+
+echo -e "";
+echo -e "";
+echo -e ${grn}"$$$$$$$$\                                            $$\       $$\ $$\   $$\ $$\   $$\ "${txtrst};
+echo -e ${grn}"$$  _____|                                           $$ |      $$ |$$ |  $$ |$$ |  $$ |"${txtrst};
+echo -e ${grn}"$$ |      $$$$$$\$$$$\   $$$$$$\   $$$$$$\  $$$$$$\  $$ | $$$$$$$ |$$ |  $$ |\$$\ $$  |"${txtrst};
+echo -e ${grn}"$$$$$\    $$  _$$  _$$\ $$  __$$\ $$  __$$\ \____$$\ $$ |$$  __$$ |$$ |  $$ | \$$$$  / "${txtrst};
+echo -e ${grn}"$$  __|   $$ / $$ / $$ |$$$$$$$$ |$$ |  \__|$$$$$$$ |$$ |$$ /  $$ |$$ |  $$ | $$  $$<  "${txtrst};
+echo -e ${grn}"$$ |      $$ | $$ | $$ |$$   ____|$$ |     $$  __$$ |$$ |$$ |  $$ |$$ |  $$ |$$  /\$$\ "${txtrst};
+echo -e ${grn}"$$$$$$$$\ $$ | $$ | $$ |\$$$$$$$\ $$ |     \$$$$$$$ |$$ |\$$$$$$$ |\$$$$$$  |$$ /  $$ |"${txtrst};
+echo -e ${grn}"\________|\__| \__| \__| \_______|\__|      \_______|\__| \_______| \______/ \__|  \__|"${txtrst};
+echo -e ${grn}"                                                                                       "${txtrst};
+echo -e ${grn}"                                                                                       "${txtrst};
+echo -e ${grn}"                                                                                       "${txtrst};
+echo -e ${grn}"                                                                                       "${txtrst};
+echo -e ${red}"                                      Version: 0.1                                     "${txtrst};
+echo -e ${red}"                                      by @100Daisy                                     "${txtrst};
+echo -e "";
+echo -e "";
+echo -e ${ylw}" √ Build is Successfully Done! "${txtrst};
+echo -e "";
+echo -e ${blu}" 〉Powered by ANDROID ™  // Ten 2019"${txtrst};
+echo -e "";
+echo -e ${red}" 〉Shine in the glory of // EmeraldUX "${txtrst};
+echo -e "";
+
